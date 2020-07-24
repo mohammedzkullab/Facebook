3e2899ded18bc4ab24c9f384a2a202a9bfcd3587
mohammed kullab <56482391+mohammedzkullab@users.noreply.github.com>
2020-07-24 14:06:00 +0300

    Add files via upload

---
+++ learning-js.js
@@ -0,0 +1,12 @@
+/*js selectors*/
+
+document.getElementById();// selects html element by its id 
+document.getElementsByClassName();// selects html element by its class name 
+document.getElementsByTagName();// selects html element with the spesific tag
+document.querySelector();//selects the first element of html elements by either its name or its class name (.className) or id (#Id)
+document.querySelectorAll();//selects All elements of html elements by either its name or its class name (.className) or id (#Id)
+
+
+/*to convert an html collection to an array */
+let test ;
+test = Array.from(test)
